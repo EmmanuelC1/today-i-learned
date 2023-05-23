259dd11631d19ff1e9f8bf46450f3b4374e02126
EmmanuelC1 <emcastillo@csumb.edu>
2023-05-22 21:27:13 -0700

    created first react component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import './style.css';
+
 function App() {
-  return [<h1>Hello World</h1>, <p>This is my first React application</p>];
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" height="68" width="68" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-share-fact">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
