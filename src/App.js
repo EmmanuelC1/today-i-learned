4239ef4f010794192b42391f2471306a3bf5b82a
EmmanuelC1 <emcastillo@csumb.edu>
2023-05-23 19:25:02 -0700

    adds config file and Category button Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import './style.css';
-
-const CATEGORIES = [
-  { name: 'technology', color: '#3b82f6' },
-  { name: 'science', color: '#16a34a' },
-  { name: 'finance', color: '#ef4444' },
-  { name: 'society', color: '#eab308' },
-  { name: 'entertainment', color: '#db2777' },
-  { name: 'health', color: '#14b8a6' },
-  { name: 'history', color: '#f97316' },
-  { name: 'news', color: '#8b5cf6' },
-];
+import ConfigData from './config/config.json';
 
 const initialFacts = [
   {
@@ -77,7 +67,26 @@ function NewFactForm() {
 }
 
 function CategoryFilter() {
-  return <aside>Category Filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li key="all" className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+
+        {ConfigData.CATEGORIES.map(cat => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactList() {
@@ -113,8 +122,9 @@ function Fact({ fact }) {
       <span
         className="tag"
         style={{
-          backgroundColor: CATEGORIES.find(cat => cat.name === fact.category)
-            .color,
+          backgroundColor: ConfigData.CATEGORIES.find(
+            cat => cat.name === fact.category
+          ).color,
         }}
       >
         {fact.category}
